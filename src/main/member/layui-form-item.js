09bffa1b0ad7771ffe7b34edcc935f24b169179c
lunkung <1522835776@qq.com>
2020-06-14 16:04:21 +0800

    add btn dymaic css

--- src/main/member/layui-form-item.js
+++ src/main/member/layui-form-item.js
@@ -9,6 +9,8 @@ class LayuiFormItem extends Component {
         this.state = {
             focused: false,
         };
+        this.handleBtnFocuse = this.handleBtnFocuse.bind(this);
+        this.handleBtnBlur = this.handleBtnBlur.bind(this);
     }
 
     render() {
@@ -17,10 +19,27 @@ class LayuiFormItem extends Component {
                 <div className="layui-input-inline">
                     <input className="layui-input" placeholder="请输入手机号码后四位"/>
                 </div>
-                <button className="layui-btn">检索</button>
+                <button className={this.state.focused ? 'layui-btn-focused': 'layui-btn'}
+                        onMouseOver={this.handleBtnFocuse}
+                        onMouseOut={this.handleBtnBlur}
+                >
+                    检索
+                </button>
             </div>
         );
     }
+
+    handleBtnFocuse(){
+        this.setState({
+            focused:true
+        });
+    }
+    handleBtnBlur(){
+        this.setState({
+            focused:false
+        });
+    }
+
 }
 
 export default LayuiFormItem;
